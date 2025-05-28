2c64c0ee82a08bb402bf061cd52e6810ef56d530
ampilda <fluttershyred1926@gmail.com>
2025-05-28 22:39:01 +0300

    up

---
+++ patch/include.c
@@ -0,0 +1,31 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+#include "bar.c"
+
+#include "bar_ltsymbol.c"
+#include "bar_status.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+#include "bar_awesomebar.c"
+#include "bar_systray.c"
+#include "bar_wintitleactions.c"
+
+/* Other patches */
+#include "alttab.c"
+#include "attachx.c"
+#include "autostart.c"
+#include "floatpos.c"
+#include "pertag.c"
+#include "restartsig.c"
+#include "sticky.c"
+#include "togglefullscreen.c"
+#include "vanitygaps.c"
+#include "xrdb.c"
+#include "seamless_restart.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_fibonacci.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
---
+++ patch/include.h
@@ -0,0 +1,30 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_awesomebar.h"
+#include "bar_systray.h"
+#include "bar_wintitleactions.h"
+
+/* Other patches */
+#include "alttab.h"
+#include "attachx.h"
+#include "autostart.h"
+#include "floatpos.h"
+#include "pertag.h"
+#include "restartsig.h"
+#include "seamless_restart.h"
+#include "sticky.h"
+#include "togglefullscreen.h"
+#include "vanitygaps.h"
+#include "xrdb.h"
+/* Layouts */
+#include "layout_fibonacci.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
